07a4b0f822d2f28e21da34cf3405ae9e5ebd271a
Kibiwott <bk.kibiwott@gmail.com>
2025-10-06 23:33:01 +0300

    Updated

---
+++ src/config.ts
@@ -0,0 +1,47 @@
+import dotenv from 'dotenv';
+import assert from 'assert';
+import sql from 'mssql';
+
+dotenv.config(); // Load environment variables from .env file
+
+const {
+SQL_SERVER, SQL_USER, SQL_PWD, SQL_DB,SQL_ENCRYPT,PORT 
+}=process.env;
+
+
+const port=PORT 
+assert(SQL_SERVER, 'SQL_SERVER is not set in environment variables');
+assert(SQL_USER, 'SQL_USER is not set in environment variables');
+assert(SQL_PWD, 'SQL_PWD is not set in environment variables');
+assert(SQL_DB, 'SQL_DB is not set in environment variables');
+assert(SQL_ENCRYPT, 'SQL_ENCRYPT is not set in environment variables');
+
+export const config = {
+    port:port,
+    sqlConfig: {
+        user: SQL_USER,
+        password: SQL_PWD,
+        database: SQL_DB,
+        server: SQL_SERVER,
+        // pool used to manage multiple connections
+        pool: {
+            max: 10,
+            min: 0,
+            IdleDeadlineTimeoutMillis: 30000
+        },
+        options: {
+            encrypt:true, 
+            trustServerCertificate: true 
+        }
+    }
+
+};
+
+export const getPool = async () => {
+    try {
+        const pool = await sql.connect(config.sqlConfig);
+        return pool;
+    } catch (error) {
+        
+    }
+}
