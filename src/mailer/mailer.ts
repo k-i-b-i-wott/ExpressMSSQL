94c860f2ca524a4b9e3879dd7d464eb531c4781b
Kibiwott <bk.kibiwott@gmail.com>
2025-10-17 16:33:54 +0300

    Updated

--- src/mailer/mailer.ts
+++ src/mailer/mailer.ts
@@ -1,28 +1,33 @@
 import dotenv from 'dotenv';
 import nodemailer from 'nodemailer';
+
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: 587,
-  secure: false,
+  port: 465,
+  service: 'gmail',
+  secure: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD,
   },
 });
-export const sendMail = async (to: string, subject: string, text: string) => {
+export const sendMail = async (to: string, subject: string, html?: string) => {
   const mailOptions = {
     from: process.env.MAIL_USER,
     to,
     subject,
-    text,
+    html: html 
   };
 
   try {
-    await transporter.sendMail(mailOptions);
-    console.log('Email sent successfully');
-  } catch (error) {
-    console.error('Error sending email:', error);
+   const mailRes = await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully', mailRes);
+    if (mailRes.accepted.length > 0) return "Email sent successfully";
+    if (mailRes.rejected.length > 0) return "Email not sent";
+    return "Email server rejected the email";
+ } catch (error:any) {
+  return JSON.stringify({message: error.message} );     
   }
 };
